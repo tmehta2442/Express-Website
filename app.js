cf3ed15925ee9798c07e8942faba32f565fa1d6e
tmehta2442 <tmehta@post.harvard.edu>
2016-05-04 23:56:32 -0700

    node mailer techncially working, will make it better

--- app.js
+++ app.js
@@ -20,5 +20,44 @@ app.get('/about', function(req, res){
 	res.render('about');
 });
 
+app.get('/contact', function(req, res){
+	res.render('contact');
+});
+
+app.post('/contact/send', function(req, res){
+	var transporter = nodemailer.createTransport({
+		service: 'Gmail',
+		auth: {
+			user: 'tejasnodeapp',
+			pass: '*******'
+		}
+	});
+
+	var mailOptions = {
+		from: 'Tejas Mehta Node <tejasnodeapp@gmail.com',
+		to: 'tj@tejas-mehta.com',
+		subject: 'Node App',
+		text: 'You have a submission with the following details... Name: ' + req.body.name + 'Email: ' + req.body.email + 'Message: ' + req.body.message,
+		html: '<p>You have a submission with the following details...</p><ul><li>Name: ' + req.body.name + '</li><li>Email: ' + req.body.email + '</li><li>Message: ' + req.body.message +'</li></ul>'
+	}
+
+	transporter.sendMail(mailOptions, function(error, info){
+		if(error) {
+			console.log(error);
+			res.redirect('/');
+		} else {
+			console.log('Message Sent: ' + info.response);
+			res.redirect('/');
+		}
+	});
+});
+
 app.listen(3000);
 console.log('Server is running on port 3000');
+
+
+
+
+
+
+
